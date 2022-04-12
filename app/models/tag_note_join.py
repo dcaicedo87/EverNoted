08eb110d3ee1154549ac877d78fd14fcc219b7d3
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-04-12 17:53:28 -0400

    added notebooks seeder

--- app/models/tag_note_join.py
+++ app/models/tag_note_join.py
@@ -7,7 +7,6 @@ class Tag_Note_Join(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tag_id = db.Column(db.Integer, db.ForeignKey("tags.id", ondelete="CASCADE"), nullable=False)
     note_id = db.Column(db.Integer, db.ForeignKey("notes.id", ondelete="CASCADE"), nullable=False)
-    dummy_column = db.Column(db.String)
 
 
     def to_dict(self):
