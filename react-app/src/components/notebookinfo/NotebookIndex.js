f9144332c1312eb72425af791d8f334b4b633612
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-04-15 15:04:44 -0400

    working on styling for notes page

--- react-app/src/components/notebookinfo/NotebookIndex.js
+++ react-app/src/components/notebookinfo/NotebookIndex.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 
 
 
@@ -7,10 +7,33 @@ const NotebookIndex = () => {
     const sessionUser = useSelector(state => state.session.user);
 
     return (
-        <div className="notebook-index">
-            <div className>
-
+        <div className="notebook-index-container">
+            <div className="notebook-index-header">
+                <h1>Notebooks</h1>
+                <div className="notebooks-index-notebooks-info">
+                    <h2># notebooks</h2>
+                    <button className="notebooks-index-notebooks-button">
+                        New Notebook
+                    </button>
+                </div>
+            </div>
+            <div className="notebooks-index-list">
+                <ul>
+                    <li>
+                        <div className="index-list-title"> TITLE</div>
+                        <div className="index-list-created">CREATED BY</div>
+                        <div className="index-list-updated">UPDATED</div>
+                        <div className="index-list-actions">ACTIONS</div>
+                    </li>
+                    <li>test2</li>
+                    <li>test3</li>
+                    <li>test4</li>
+                    <li>test5</li>
+                    <li>test6</li>
+                </ul>
             </div>
         </div>
     )
 }
+
+export default NotebookIndex;
