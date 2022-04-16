ca3c12315230f1551cb47152dc2404cb1d04a361
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-04-15 22:18:22 -0400

    added conditional to class for main container.. checks to see if user is logged in.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -39,40 +39,42 @@ function App() {
   }
 
   return (
-    <div className="main-container" id="user-signup">
-      <BrowserRouter>
-        <UserNavBar />
-        <Switch>
-          <Route path='/login' exact={true}>
-            <LoginForm />
-          </Route>
-          <Route path='/sign-up' exact={true}>
-            <SignUpForm />
-          </Route>
-          <ProtectedRoute path='/users' exact={true} >
-            <UsersList/>
-          </ProtectedRoute>
-          <ProtectedRoute path='/users/:userId' exact={true} >
-            <User />
-          </ProtectedRoute>
-          <ProtectedRoute path='/notes/:noteId' exact={true} >
-            <>
+    <>
+      <div className={`main-container${!sessionUser ? "-login": ""}`}>
+        <BrowserRouter>
+          <UserNavBar />
+          <Switch>
+            <Route path='/login' exact={true}>
+              <LoginForm />
+            </Route>
+            <Route path='/sign-up' exact={true}>
+              <SignUpForm />
+            </Route>
+            <ProtectedRoute path='/users' exact={true} >
+              <UsersList/>
+            </ProtectedRoute>
+            <ProtectedRoute path='/users/:userId' exact={true} >
+              <User />
+            </ProtectedRoute>
+            <ProtectedRoute path='/notes/:noteId' exact={true} >
+              <>
+                <NoteInfoSide />
+                <NoteInfoEdit />
+              </>
+            </ProtectedRoute>
+            <ProtectedRoute path="/notes" exact={true}>
               <NoteInfoSide />
-              <NoteInfoEdit />
-            </>
-          </ProtectedRoute>
-          <ProtectedRoute path="/notes" exact={true}>
-            <NoteInfoSide />
-          </ProtectedRoute>
-          <ProtectedRoute path="/notebooks" exact={true}>
-            <NotebookIndex />
-          </ProtectedRoute>
-          <ProtectedRoute path='/' exact={true} >
-            <LoginForm />
-          </ProtectedRoute>
-        </Switch>
-      </BrowserRouter>
-    </div>
+            </ProtectedRoute>
+            <ProtectedRoute path="/notebooks" exact={true}>
+              <NotebookIndex />
+            </ProtectedRoute>
+            <ProtectedRoute path='/' exact={true} >
+              <LoginForm />
+            </ProtectedRoute>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
